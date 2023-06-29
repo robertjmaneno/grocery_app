5772d83d3d755d9aac524d2188790e16c802dcd5
Robert J Maneno <robertjmaneno@gmail.com>
2023-06-29 15:11:30 +0200

    finishing the theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:grocery_app/buyer/screens/homescreen.dart';
-import 'package:grocery_app/consts/theme_data.dart';
+import 'package:grocery_app/provider/dark_theme_provider.dart';
+import 'package:grocery_app/screens/buyer/home_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'consts/theme_data.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.setDarkTheme =
+        await themeChangeProvider.darkThemePrefs.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    bool isDark = true;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Welcome',
-      theme: Styles.themeData(true, context),
-      home: const HomeScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) {
+          return themeChangeProvider;
+        })
+      ],
+      child:
+          Consumer<DarkThemeProvider>(builder: (context, themeProvider, child) {
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: Styles.themeData(themeProvider.getDarkTheme, context),
+            home: HomeScreen());
+      }),
     );
   }
 }
